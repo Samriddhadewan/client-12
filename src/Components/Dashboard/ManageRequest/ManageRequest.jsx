23b71d381a52a706b491c4064dc35e1a03411352
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-05-13 15:10:04 +0600

    payment confirmation related all functionality added

--- src/Components/Dashboard/ManageRequest/ManageRequest.jsx
+++ src/Components/Dashboard/ManageRequest/ManageRequest.jsx
@@ -4,10 +4,36 @@ import SectionTitle from "../../SectionTitle/SectionTitle";
 import { FaMessage } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import { LuMessageSquareOff } from "react-icons/lu";
+import { TbTrashOff } from "react-icons/tb";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
 const ManageRequest = () => {
-  const [data] = useMyRequest();
-  console.log(data);
+  const axiosSecure = useAxiosSecure();
+  const [data, refetch] = useMyRequest();
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure
+          .delete(`/request-delete/user/${item?._id}?campId=${item?.camp_id}`)
+          .then((res) => {
+            if (res.data.deletedCount > 0) {
+              refetch();
+              toast.success("request deleted Successfully");
+            }
+          });
+      }
+    });
+  };
   return (
     <div className="px-6">
       <SectionTitle
@@ -52,11 +78,36 @@ const ManageRequest = () => {
                     </>
                   )}
                 </td>
-                <td>{request?.confirmation_status}</td>
                 <td>
-                  <p className="text-xl text-red-500">
-                    <FaTrash></FaTrash>
-                  </p>
+                  {request?.confirmation_status === "pending" ? (
+                    <>
+                      <p className="text-red-500">
+                        {request?.confirmation_status}
+                      </p>
+                    </>
+                  ) : (
+                    <>
+                      <p className="text-green-500">
+                        {request?.confirmation_status}
+                      </p>
+                    </>
+                  )}
+                </td>
+                <td>
+                  {request?.payment_status === "paid" ? (
+                    <>
+                      <p onClick={()=> toast.error("You already paid for the camp, Can't delete!")} className="text-xl text-red-500">{<TbTrashOff />}</p>
+                    </>
+                  ) : (
+                    <>
+                      <p
+                        onClick={() => handleDelete(request)}
+                        className="text-xl text-red-500"
+                      >
+                        <FaTrash></FaTrash>
+                      </p>
+                    </>
+                  )}
                 </td>
                 <td>
                   {request?.confirmation_status === "confirmed" ? (
--- src/Components/Dashboard/ManageRequstCamps/ManageRequestCamps.jsx
+++ src/Components/Dashboard/ManageRequstCamps/ManageRequestCamps.jsx
@@ -2,9 +2,47 @@ import React from "react";
 import SectionTitle from "../../SectionTitle/SectionTitle";
 import useAllRequests from "../../../Hooks/useAllRequests";
 import { FaTrash } from "react-icons/fa";
+import { TbTrashOff } from "react-icons/tb";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import toast from "react-hot-toast";
+import Swal from "sweetalert2";
 
 const ManageRequestCamps = () => {
-  const [data] = useAllRequests();
+  const axiosSecure = useAxiosSecure();
+  const [data, refetch] = useAllRequests();
+  const handleConfirm = async (item) => {
+    if (item.payment_status === "unpaid") {
+      toast.error("Cannot confirm. Payment is still unpaid.");
+      return;
+    }
+    axiosSecure.patch(`/request-confirm/${item?._id}`).then((res) => {
+      if (res.data.modifiedCount > 0) {
+        refetch();
+        toast.success("Request Confirm Success");
+      }
+    });
+  };
+  const handleDelete = async (item) => {
+    console.log(item)
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/request-delete/admin/${item._id}?campId=${item?.camp_id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            toast.success("successfully deleted the request");
+          }
+        });
+      }
+    });
+  };
 
   return (
     <div className="px-6">
@@ -34,12 +72,52 @@ const ManageRequestCamps = () => {
                 <td>{item?.participant_name}</td>
                 <td>{item?.camp_name}</td>
                 <td>{item?.fees}$</td>
-                <td>{item?.payment_status}</td>
-                <td>{item?.confirmation_status}</td>
                 <td>
-                  <p className="text-xl text-red-600">
-                    <FaTrash></FaTrash>
-                  </p>
+                  {item?.payment_status === "paid" ? (
+                    <>
+                      <p className="text-green-500">{item?.payment_status}</p>
+                    </>
+                  ) : (
+                    <>
+                      <p className="text-red-500">{item?.payment_status}</p>
+                    </>
+                  )}
+                </td>
+                <td>
+                  {item?.confirmation_status === "pending" ? (
+                    <>
+                      <button
+                        onClick={() => handleConfirm(item)}
+                        className="btn "
+                      >
+                        {item?.confirmation_status}
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <p className="text-green-400">
+                        {item?.confirmation_status}
+                      </p>
+                    </>
+                  )}
+                </td>
+                <td>
+                  {item?.payment_status === "paid" ? (
+                    <>
+                      <p onClick={()=>toast.error("You already confirmed the request,can't delete!")} className="text-red-600 text-xl">
+                        <TbTrashOff />
+                      </p>
+                    </>
+                  ) : (
+                    <>
+                      <p
+                        onClick={() => handleDelete(item)}
+                        className="text-xl text-red-600"
+                      >
+                        <FaTrash></FaTrash>
+                      </p>
+                    </>
+                  )}
                 </td>
               </tr>
             ))}
